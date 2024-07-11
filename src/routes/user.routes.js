867ef58a3e0f3f9da1a1056d24bc64d621bef3fe
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-07-11 15:54:22 +0530

    authentication part done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,17 +3,11 @@ import {
     loginUser, 
     logoutUser, 
     registerUser, 
-    refreshAccessToken, 
-    changeCurrentPassword, 
-    getCurrentUser, 
-    updateUserAvatar, 
-    updateUserCoverImage, 
-    getUserChannelProfile, 
-    getWatchHistory, 
-    updateAccountDetails,
-    verifyUser
+    verifyUser,
+    forgotPassword,
+    resetPasswordGet,
+    resetPasswordPost
 } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
@@ -25,17 +19,13 @@ router.route("/login").post(loginUser)
 
 router.route("/verify/:userId/:uniqueString").get(verifyUser)
 
+router.route("/forgot-password").post(forgotPassword)
+
+router.route("/reset-password/:userId/:token").get(resetPasswordGet)
+
+router.route("/reset-password/:userId/:token").post(resetPasswordPost)
+
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
-
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
-router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
