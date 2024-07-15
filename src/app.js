77043c16421207c313bc296f5ae39ba5b57dd288
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-07-15 15:03:57 +0530

    edited app

--- src/app.js
+++ src/app.js
@@ -11,18 +11,16 @@ const app = express();
 //      }))
 app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:5174"], 
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+
 
 app.use(express.json());
 app.use('/api/v1/products', productRoutes);
