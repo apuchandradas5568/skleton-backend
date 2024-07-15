1b45a1330523b6fb2806b8f6ec33d2a9ff883c60
Rakesh7502 <rakeshchinna3323@gmail.com>
2024-07-15 13:53:46 +0530

    added productDeatils to DB

--- src/app.js
+++ src/app.js
@@ -1,27 +1,54 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from 'express';
+import cors from 'cors'; 
+import productRoutes from './routes/product.routes.js'; 
 
-const app = express()
+const app = express();
 
+// Use cors middleware to allow cross-origin requests
+// app.use(cors({
+//          origin: process.env.CORS_ORIGIN,
+//         credentials: true
+//      }))
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json());
+app.use('/api/v1/products', productRoutes);
 
-
-//routes import
-import userRouter from './routes/user.routes.js'
+export default app;
 
 
-//routes declaration
-app.use("/api/v1/users", userRouter)
+// import express from "express"
+// import cors from "cors"
+// import cookieParser from "cookie-parser"
+// const bodyParser = require('body-parser');
 
-// http://localhost:8000/api/v1/users/register
+// const app = express()
 
-export { app }
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+// app.use(bodyParser.json());
+
+// app.use(express.json({limit: "16kb"}))
+// app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// app.use(express.static("public"))
+// app.use(cookieParser())
+
+
+// //routes import
+// import userRouter from './routes/user.routes.js'
+// const productRoutes = require('./routes/products.routes.js');
+
+
+// //routes declaration
+// app.use("/api/v1/users", userRouter)
+// app.use('/api/products', productRoutes);
+
+// // http://localhost:8000/api/v1/users/register
+
+// export { app }
---
+++ src/config/multer-config.js
@@ -0,0 +1,16 @@
+// // multer-config.js
+// import multer from 'multer';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// import cloudinary from './cloudinary.config.js';
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'products', // Cloudinary folder
+//     allowedFormats: ['jpeg', 'png', 'jpg'],
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// export default upload;
